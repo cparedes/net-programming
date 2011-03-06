76dd6cd2149ab66edf7f4362f439415a1e2ec0f2
Christian Paredes <cp@redbluemagenta.com>
2011-03-05 19:19:56 -0800

    Initial commit.

---
+++ showip/showip.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[]) {
+  struct addrinfo hints, *res, *p;
+  int status;
+  char ipstr[INET6_ADDRSTRLEN];
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: showip hostname\n");
+    return 1;
+  }
+
+  memset(&hints, 0, sizeof hints); // make sure hints is empty.
+  hints.ai_family = AF_UNSPEC; // don't care if it's v4 or v6
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((status = getaddrinfo(argv[1], NULL, &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(status));
+    return 2;
+  }
+
+  printf("IP addresses for %s:\n\n", argv[1]);
+
+  for ( p = res ; p != NULL ; p = p->ai_next) {
+    void *addr;
+    char *ipver;
+
+    // get the pointer to the address itself.  different fields in v4/v6
+    if (p->ai_family == AF_INET) {
+      struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+      addr = &(ipv4->sin_addr);
+      ipver = "IPv4";
+    } else {
+      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+      addr = &(ipv6->sin6_addr);
+      ipver = "IPv6";
+    }
+
+    // convert IP to a string and print it.
+    inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+    printf("  %s: %s\n", ipver, ipstr);
+  }
+
+  freeaddrinfo(res);  // free linked list
+
+  return 0;
+}
